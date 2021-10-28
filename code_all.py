be14866d001fd8b5d51ea19811aa9d8bc74cbd4e
chenqijin <qijin.chen.sh@gmail.com>
2021-10-28 11:45:23 +0800

    add a line set theme for the plot

--- code_all.py
+++ code_all.py
@@ -1,7 +1,8 @@
 import tushare as ts
 import pandas as pd
 import plotly.graph_objects as go
-token = 'which you need to replace with your own token'
+import plotly.io as pio
+token = '6a721053ea3e70bb52605d6c0972caeda9ff080d3671f69bd8b6b434'
 pro = ts.pro_api(token)
 df = pro.daily(ts_code='600519.SH', start_date='20000101', end_date=20211027)
 
@@ -11,6 +12,7 @@ df.index = pd.to_datetime(df.index)
 df.sort_index(inplace=True)
 
 # calculate k lines using 'open', 'high', 'low', 'close', 'vol'
+pio.templates.default = 'plotly_dark'
 fig = go.Figure(data=[
     go.Candlestick(x=df.index.strftime("%Y/%m/%d"),
                    open=df.open,
